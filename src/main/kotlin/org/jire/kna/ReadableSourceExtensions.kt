deaa68d770a7aec146627a0ae0a3a941d89e0191
Jire <thomasgnappo@gmail.com>
2020-12-16 23:47:19 -0500

    fix: `ReadableSourceExtensions` long/float/double/char return type

--- src/main/kotlin/org/jire/kna/ReadableSourceExtensions.kt
+++ src/main/kotlin/org/jire/kna/ReadableSourceExtensions.kt
@@ -12,16 +12,16 @@ fun ReadableSource.int(address: Long, offset: Long = 0) =
 	read(address, Int.SIZE_BYTES.toLong())?.getInt(offset) ?: 0
 
 fun ReadableSource.long(address: Long, offset: Long = 0) =
-	read(address, Long.SIZE_BYTES.toLong())?.getLong(offset) ?: 0
+	read(address, Long.SIZE_BYTES.toLong())?.getLong(offset) ?: 0L
 
 fun ReadableSource.float(address: Long, offset: Long = 0) =
-	read(address, Float.SIZE_BYTES.toLong())?.getFloat(offset) ?: 0
+	read(address, Float.SIZE_BYTES.toLong())?.getFloat(offset) ?: 0F
 
 fun ReadableSource.double(address: Long, offset: Long = 0) =
-	read(address, Double.SIZE_BYTES.toLong())?.getDouble(offset) ?: 0
+	read(address, Double.SIZE_BYTES.toLong())?.getDouble(offset) ?: 0.0
 
 fun ReadableSource.char(address: Long, offset: Long = 0) =
-	read(address, Char.SIZE_BYTES.toLong())?.getChar(offset) ?: 0
+	read(address, Char.SIZE_BYTES.toLong())?.getChar(offset) ?: 0.toChar()
 
 fun ReadableSource.boolean(address: Long, offset: Long = 0) =
 	byte(address, offset).toInt() != 0
