c5f56c1873cd894bbc32c46dc38a5e38e0920239
Jire <thomasgnappo@gmail.com>
2020-12-21 12:32:52 -0500

    fix: `WindowsAttachedProcess` read/write size 8 override bug

--- src/main/kotlin/org/jire/kna/attach/windows/WindowsAttachedProcess.kt
+++ src/main/kotlin/org/jire/kna/attach/windows/WindowsAttachedProcess.kt
@@ -1,8 +1,8 @@
 package org.jire.kna.attach.windows
 
+import com.sun.jna.Memory
 import com.sun.jna.Pointer
 import com.sun.jna.platform.win32.WinNT
-import org.jire.kna.MemoryCache
 import org.jire.kna.attach.AbstractAttachedProcess
 import org.jire.kna.nativelib.windows.NTDLL
 
@@ -10,15 +10,17 @@ class WindowsAttachedProcess(val handle: WinNT.HANDLE) : AbstractAttachedProcess
 	
 	override val modules = WindowsAttachedModules()
 	
+	// we need these separate so it doesn't override writes of size 8
+	private val readMemory = Memory(8)
+	private val writtenMemory = Memory(8)
+	
 	override fun read(address: Pointer, data: Pointer, bytesToRead: Long): Boolean {
-		val readMemory = MemoryCache[8]
 		NTDLL.NtReadVirtualMemory(handle.pointer, address, data, bytesToRead, readMemory)
 		val readBytes = readMemory.getLong(0)
 		return readBytes != 0L
 	}
 	
 	override fun write(address: Pointer, data: Pointer, bytesToWrite: Long): Boolean {
-		val writtenMemory = MemoryCache[8]
 		NTDLL.NtWriteVirtualMemory(handle.pointer, address, data, bytesToWrite, writtenMemory)
 		val writtenBytes = writtenMemory.getLong(0)
 		return writtenBytes != 0L
