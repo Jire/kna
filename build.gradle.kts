9aecc7511382c785e06599b7376911c4dedfe2ae
Jire <thomasgnappo@gmail.com>
2020-12-21 12:38:17 -0500

    build: remove Kotlin compile JVM target, and bump version to 0.2.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,10 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
 plugins {
 	kotlin("jvm") version "1.4.21"
 	id("com.github.dcendents.android-maven") version "2.1" apply true
 }
 
 group = "org.jire"
-version = "0.1.5"
+version = "0.2.0"
 
 repositories {
 	jcenter()
@@ -18,8 +16,4 @@ dependencies {
 	implementation("net.java.dev.jna", "jna", jnaVersion)
 	implementation("net.java.dev.jna", "jna-platform", jnaVersion)
 	implementation("it.unimi.dsi", "fastutil", "8.4.4")
-}
-
-tasks.withType<KotlinCompile> {
-	kotlinOptions.jvmTarget = "13"
 }
