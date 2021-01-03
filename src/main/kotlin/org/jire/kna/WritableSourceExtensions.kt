78ebbcd7eda330ee2093417526d4f8725fbd72bc
Jire <thomasgnappo@gmail.com>
2021-01-03 15:24:34 -0500

    fix: writing extensions to use offsetted address

--- src/main/kotlin/org/jire/kna/WritableSourceExtensions.kt
+++ src/main/kotlin/org/jire/kna/WritableSourceExtensions.kt
@@ -2,44 +2,14 @@
 
 package org.jire.kna
 
-import net.openhft.chronicle.core.OS
+inline fun WritableSource.set(address: Long, size: Long, writeToPointer: Pointer.() -> Unit) =
+	write(offset(address), PointerCache[size].apply(writeToPointer), size)
 
-operator fun WritableSource.set(address: Long, value: Byte) {
-	val writePointer = PointerCache[1]
-	OS.memory().writeByte(writePointer.address, value)
-	write(address, writePointer, 1)
-}
-
-operator fun WritableSource.set(address: Long, value: Short) {
-	val writePointer = PointerCache[2]
-	OS.memory().writeShort(writePointer.address, value)
-	write(address, writePointer, 2)
-}
-
-operator fun WritableSource.set(address: Long, value: Int) {
-	val writePointer = PointerCache[4]
-	OS.memory().writeInt(writePointer.address, value)
-	write(address, writePointer, 4)
-}
-
-operator fun WritableSource.set(address: Long, value: Long) {
-	val writePointer = PointerCache[8]
-	OS.memory().writeLong(writePointer.address, value)
-	write(address, writePointer, 8)
-}
-
-operator fun WritableSource.set(address: Long, value: Float) {
-	val writePointer = PointerCache[4]
-	OS.memory().writeFloat(writePointer.address, value)
-	write(address, writePointer, 4)
-}
-
-operator fun WritableSource.set(address: Long, value: Double) {
-	val writePointer = PointerCache[8]
-	OS.memory().writeDouble(writePointer.address, value)
-	write(address, writePointer, 8)
-}
-
+operator fun WritableSource.set(address: Long, value: Byte) = set(address, 1) { setByte(0, value) }
+operator fun WritableSource.set(address: Long, value: Short) = set(address, 2) { setShort(0, value) }
+operator fun WritableSource.set(address: Long, value: Int) = set(address, 4) { setInt(0, value) }
+operator fun WritableSource.set(address: Long, value: Long) = set(address, 8) { setLong(0, value) }
+operator fun WritableSource.set(address: Long, value: Float) = set(address, 4) { setFloat(0, value) }
+operator fun WritableSource.set(address: Long, value: Double) = set(address, 1) { setDouble(0, value) }
 operator fun WritableSource.set(address: Long, value: Char) = set(address, value.toShort())
-
 operator fun WritableSource.set(address: Long, value: Boolean) = set(address, (if (value) 1 else 0).toByte())
